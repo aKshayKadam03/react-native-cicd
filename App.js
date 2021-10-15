f69b9112dc71762332511ecb97dbc451d2d2ca61
therealdrtroll <akshaykadam7991@gmail.com>
2021-10-15 14:53:51 +0530

    tracking events

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
+import React,{ useEffect } from 'react';
 
-import React from 'react';
+import {SafeAreaView, Text, Button} from 'react-native';
 
-import {
-  SafeAreaView,
-  Text,
-} from 'react-native';
+import Carshes from 'appcenter-crashes';
+import Analytics from 'appcenter-analytics'
 
-export default function App(){
-  return <SafeAreaView>
-    <Text style={{textAlign:'center'}}>React CICD Setup</Text>
-  </SafeAreaView>
+export default function App() {
+
+  async function checkPreviousSession(){
+    const hasCrashed = await Carshes.hasCrashedInLastSession()
+    if(hasCrashed){
+      alert("Sorry about the crash, we are working on a solution")
+    }
+  }
+
+  useEffect(() => {
+    checkPreviousSession()
+  }, [])
+
+  return (
+    <SafeAreaView>
+      <Text style={{textAlign: 'center'}}>React CICD Setup</Text>
+      <Button
+        title="Crash this shit"
+        onPress={() => Analytics.trackEvent("My custom event",{ internetSpeed: '20mbps'})}
+      />
+    </SafeAreaView>
+  );
 }
